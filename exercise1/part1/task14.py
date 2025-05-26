56058eecff535fb7f1f691d0be93894e380309f4
Ala a <allan2021jose@gmail.com>
2025-05-26 15:28:39 -0300

    feito tasks 5, 13 e 14

---
+++ exercise1/part1/task14.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+list = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+print("list:")
+print(list)
+
+matrix = list.reshape(3, 4)
+print("matrix:")
+print(matrix)
+
+#task 13:
+
+listAgain = matrix.reshape(-1)
+print("list again:")
+print(listAgain)
+
+#task 14:
+
+matrixAgain = listAgain.reshape(2, 2, 3)
+print("matrix again:")
+print(matrixAgain)
---
+++ exercise1/part1/task5.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a = np.arange(0, 50, 5)
+
+print(a)
