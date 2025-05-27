393a92db05e67d844cdc3c6f62bef3e4acf10538
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-05-26 23:14:43 -0300

    Tarefa 15 resolvida

---
+++ exercise1/part1/task15.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+array = np.arange(21)
+
+print("Elemento no índice 3:")
+print(array[3])
+
+print("Ultimo elemento do array:")
+print(array[-1])
+
+print("Elementos do índice 3 ao 9:")
+print(array[3:9])
