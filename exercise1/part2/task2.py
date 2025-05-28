5e43a2aa09fbf91f627a63537ddcaad51d27b373
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-05-28 12:07:44 -0300

    /part2/task2 resolvida

--- exercise1/part2/task2.py
+++ exercise1/part2/task2.py
@@ -1,14 +1,17 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy import datasets
 
 img = datasets.face()
 
+gray = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
+
 def main():
-    plt.imshow(img)
+    plt.imshow(gray, cmap='gray')
     plt.axis('off')
-    plt.savefig('face.png')
+    plt.savefig('face_gray.png') 
 
 if __name__ == '__main__':
     main()
