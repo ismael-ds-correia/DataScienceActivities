5467e980845a6265f7b7c73759260632697e59e9
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-05-28 15:29:15 -0300

    /part3/task2_2 resolvida

---
+++ exercise1/part3/task2_2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = [
+    {'TIME': 'Liverpool', 'PAÍS': 'Inglaterra', 'TORCEDORES': 60456478, 'MÉDIA SALARIAL': 198000},
+    {'TIME': 'Santa Cruz', 'PAÍS': 'Brasil', 'TORCEDORES': 951, 'MÉDIA SALARIAL': 10000},
+    {'TIME': 'Milan', 'PAÍS': 'Itália', 'TORCEDORES': 890765667, 'MÉDIA SALARIAL': 110369},
+    {'TIME': 'Frankfurt', 'PAÍS': 'Alemanha', 'TORCEDORES': 149087, 'MÉDIA SALARIAL': 97236},
+    {'TIME': 'All Nassr', 'PAÍS': 'Arabia Saudita', 'TORCEDORES': 30000, 'MÉDIA SALARIAL': 214236}
+]
+
+df = pd.DataFrame(data)
+
+print(df.head(2))
