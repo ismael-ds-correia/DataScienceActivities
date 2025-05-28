43cbf4a08904433c867afe85cbdb0c4e5a7e5e8a
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-05-28 12:02:12 -0300

    Alteração em part2/task1

--- exercise1/part2/task1.py
+++ exercise1/part2/task1.py
@@ -1,12 +1,14 @@
-from scipy import datasets
+import matplotlib
+matplotlib.use('Agg')         # <-- escolhe um backend “headless”
 import matplotlib.pyplot as plt
+from scipy import datasets
 
 img = datasets.face()
 
 def main():
     plt.imshow(img)
     plt.axis('off')
-    plt.show()
+    plt.savefig('face.png')  # salva em arquivo em vez de plt.show()
 
 if __name__ == '__main__':
     main()
