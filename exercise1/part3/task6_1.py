01762e136dbd16c1151a9a1359f092db56de5e46
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-05-28 23:07:11 -0300

    /part3/task6_1 resolvida

---
+++ exercise1/part3/task6_1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data1 = {
+    'TIME': ['Campinense', 'Naútico', 'Santos'],
+    'TORCEDORES': [604564, 765298, 890765]
+}
+
+teams_df1 = pd.DataFrame(data1)
+
+data2 = {
+    'TIME': ['Vitoria', 'Internacional'],
+    'TORCEDORES': [1920613, 1678943]
+}
+
+teams_df2 = pd.DataFrame(data2)
+
+print(teams_df1)
+print(teams_df2)
