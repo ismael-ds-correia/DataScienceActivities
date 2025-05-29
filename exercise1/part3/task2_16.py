e1f99dc0f37a556c2b2c5252d6c2be860e30afec
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-05-28 21:41:43 -0300

    /part3/task2_16 resolvida

---
+++ exercise1/part3/task2_16.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+data = [
+    {'TIME': 'Liverpool', 'PAÍS': 'Inglaterra', 'TORCEDORES': 60456478, 'MÉDIA SALARIAL': 198000},
+    {'TIME': 'Santa Cruz', 'PAÍS': 'Brasil', 'TORCEDORES': 951, 'MÉDIA SALARIAL': 10000},
+    {'TIME': 'Milan', 'PAÍS': 'Itália', 'TORCEDORES': 890765667, 'MÉDIA SALARIAL': 110369},
+    {'TIME': 'Frankfurt', 'PAÍS': 'Alemanha', 'TORCEDORES': 149087, 'MÉDIA SALARIAL': 97236},
+    {'TIME': 'All Nassr', 'PAÍS': 'Arabia Saudita', 'TORCEDORES': 30000, 'MÉDIA SALARIAL': 214236}
+]
+
+df = pd.DataFrame(data)
+
+df['TORCEDORES'] = df['TORCEDORES'].astype(float)
+
+dates = [
+    '15/03/2010',
+    '22/07/2015',
+    '05/11/2008',
+    '18/04/2022',
+    '30/09/2019'
+]
+
+df['DATA'] = dates
+
+df = df.rename(columns={'TORCEDORES': 'Nº TORCEDORES', 'DATA': 'DATA - CRIAÇÃO'})
+
+selected_columns = df[['PAÍS', 'MÉDIA SALARIAL', 'Nº TORCEDORES']]
+
+first_rows_columns = selected_columns.iloc[0:2, 0:3]
+
+print(selected_columns)
