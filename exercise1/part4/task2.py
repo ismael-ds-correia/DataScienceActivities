8eb55f798021bb07217d43acf5f7ea1e90bbf27f
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-05-29 14:16:55 -0300

    Adicionada normalização dos dados

--- exercise1/part4/task2.py
+++ exercise1/part4/task2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, OneHotEncoder
 
 # Carregar o dataset
 penguins = sns.load_dataset("penguins")
@@ -29,3 +29,15 @@ penguins_encoded = pd.get_dummies(penguins, columns=['species', 'island'], drop_
 # print(penguins_encoded.head())
 
 penguins_encoded.to_csv('penguins_treated.csv', index=False)
+
+# 3. NORMALIZAÇÃO DAS COLUNAS NUMÉRICAS (valores entre 0 e 1)
+
+# Inicializando o MinMaxScaler
+scaler = MinMaxScaler()
+
+# Aplicando a normalização
+penguins_encoded[numeric_cols] = scaler.fit_transform(penguins_encoded[numeric_cols])
+
+# print(penguins_encoded[numeric_cols].head())
+
+penguins_encoded.to_csv('penguins_normalized.csv', index=False)
