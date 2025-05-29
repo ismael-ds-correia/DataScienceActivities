92e33a12de28ba49299baafc2a1cdd6eba48a9f0
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-05-29 12:53:51 -0300

    Verificando existência de valores nulos após tratamento e atestada a corretude do tratamento

--- exercise1/part4/task2.py
+++ exercise1/part4/task2.py
@@ -8,7 +8,16 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 penguins = sns.load_dataset("penguins")
 
 # 1. TRATAMENTO DE VALORES NULOS
-# Para colunas numéricas: preenchendo com a mediana,
+# Para colunas numéricas: preenchendo com a mediana.
 numeric_cols = ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']
 for col in numeric_cols:
     penguins[col].fillna(penguins[col].median(), inplace=True)
+
+# Para colunas categóricas: preenchendo com o a moda.
+categorical_cols = ['species', 'island', 'sex']
+for col in categorical_cols:
+    penguins[col].fillna(penguins[col].mode()[0], inplace=True)
+
+# Verificar se ainda existem valores nulos
+print("\nValores nulos após tratamento:")
+print(penguins.isnull().sum())
